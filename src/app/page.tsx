71b314bfdad93114e87919f3ebfb846a0381764f
Shreyanshu Jaiswal <Shreyanshu.jais@gmail.com>
2025-08-25 21:02:42 +0000

    remove featured work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import HeroSection from '@/components/sections/hero';
 import ServicesSection from '@/components/sections/services';
-import PortfolioSection from '@/components/sections/portfolio';
 import ContactSection from '@/components/sections/contact';
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <main className="flex-grow">
         <HeroSection />
         <ServicesSection />
-        <PortfolioSection />
         <ContactSection />
       </main>
       <Footer />
